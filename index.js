e360c4996974fb6a6c9e029428e418027330c5c1
anissa guembre <guembreani@gmail.com>
2021-04-11 22:30:52 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(name,type){
+    const cards = [["lisa", "kaitlin", "jan"], "surprise"];
+    for (let i =0; i < name.length; i++){
+        cards[i] = `Thank you, ${name[i]}, for the wonderful ${type} gift!`
+ 
+     }
+     console.log (cards)
+     return cards;
+
+}
+writeCards(["lisa", "kaitlin", "jan"], "surprise");
+
+function countDown(i) {
+   
+    while(i >=0) {
+        console.log(i);
+        i--;
+    }
+}
+
+countDown(10);
